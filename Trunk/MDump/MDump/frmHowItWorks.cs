efbb2f890d22499168106f8151b445f4ffed7f7e
Matt Kline <slavik262@gmail.com>
2011-01-01 10:11:24 -0600

    Forgot to add some new files.
    Signed-off-by: Matt Kline <slavik262@gmail.com>

---
+++ Trunk/MDump/MDump/frmHowItWorks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MDump
+{
+    public partial class frmHowItWorks : Form
+    {
+        public frmHowItWorks()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
