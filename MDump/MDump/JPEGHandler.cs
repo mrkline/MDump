3471549934678cf30c450f6b4b1b64b76102671a
Matt Kline <slavik262@gmail.com>
2011-02-17 11:00:39 -0600

    Upgraded to .NET 3.0 for JpegBitmapEncoder class. This removes Windows 2000 compatibility. Added JPEGHandler to handle JPEG splits and merges.
    Signed-off-by: Matt Kline <slavik262@gmail.com>

---
+++ MDump/MDump/JPEGHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDump
+{
+    class JPEGHandler : ImageFormatHandler
+    {
+        public string FormatName
+        {
+            get { return "JPEG"; }
+        }
+
+        public bool SupportsMergedImage(string filepath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] LoadMergedImageData(string filepath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] SaveToMemory(System.Drawing.Bitmap bitmap, byte[] mdData, MDumpOptions.CompressionLevel compLevel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
